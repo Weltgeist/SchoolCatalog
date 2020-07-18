0964b347a8fa8b7be203fb5f37f0aeb0272b8e46
Weltgeist <narcisse118@gmail.com>
2020-07-18 17:44:39 -0400

    added class instance code

--- app.js
+++ app.js
@@ -63,3 +63,9 @@ class High extends School {
   }
 
 }
+
+const lorraineHansbury = new Primary('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']));
+const alSmith = new High('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+console.log(alSmith.sportsTeams);
